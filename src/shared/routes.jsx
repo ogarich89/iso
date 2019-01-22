58dc7098ceb351e908fb421d9b11e42b83604e72
ogarich89 <ogarich89@icloud.com>
2019-01-22 22:14:31 +0300

    intermediate changes

--- src/shared/routes.jsx
+++ src/shared/routes.jsx
@@ -1,42 +1,11 @@
-import Loading from './components/_common/Loading/Loading';
 import { getProduct, getProducts } from './store/actions/products';
-import loadable from '@loadable/component';
-import React from 'react';
-const delay = 300;
+import { page } from '../shared/helpers';
+
 const routes = [
-  {
-    path: '/',
-    exact: true,
-    chunkName: 'home.page',
-    component: loadable(() => import(/* webpackChunkName: "home.page" */ './pages/home'), {
-      fallback: <Loading timer={delay}/>
-    })
-  },
-  {
-    path: '/products',
-    exact: true,
-    chunkName: 'products.page',
-    component: loadable(() => import(/* webpackChunkName: "products.page" */ './pages/products'), {
-      fallback: <Loading timer={delay}/>
-    }),
-    initialAction: (api, req = {}) => getProducts(api, req)
-  },
-  {
-    path: '/products/:product',
-    exact: true,
-    chunkName: 'product.page',
-    component: loadable(() => import(/* webpackChunkName: "product.page" */ './pages/products.product'), {
-      fallback: <Loading timer={delay}/>
-    }),
-    initialAction: (api, req = {}) => getProduct(api, req)
-  },
-  {
-    path: '*',
-    chunkName: 'page-not-found',
-    component: loadable(() => import(/* webpackChunkName: "page-not-found" */ './pages/page-not-found'), {
-      fallback: <Loading timer={delay}/>
-    })
-  }
+  page('/', 'home'),
+  page('/products', 'products', getProducts),
+  page('/products/:product', 'products.product', getProduct),
+  page('*', 'page-not-found', null, false)
 ];
 
 export default routes;
