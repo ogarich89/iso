ac813cac5484c872259fe40e077573e2d1313e3e
ogarich89 <ogarich89@icloud.com>
2022-12-12 21:26:49 +0100

    Refactoring config

--- config/config.cjs
+++ config/config.cjs
@@ -1,18 +1,8 @@
 const NODE_ENV = process.env.NODE_ENV || 'development';
-
-class CustomError extends Error {
-  constructor({ name = 'MODULE_NOT_FOUND', stack = '', message }) {
-    super();
-    super.name = `\x1b[0m${name}\x1b[0m`;
-    super.stack = stack;
-    super.message = `\x1b[41m ${message} \x1b[0m`;
-  }
-}
 try {
   const config = require(`./environment/${NODE_ENV}.json`);
   module.exports = { config };
 } catch (_) {
-  throw new CustomError({
-    message: `Configure file (/config/environment/${NODE_ENV}.json) not found. Read the instruction (README.md)`,
-  });
+  const message = `Configure file (/config/environment/${NODE_ENV}.json) not found. Read the instruction (README.md)`;
+  throw `MODULE_NOT_FOUND: \x1b[41m ${message} \x1b[0m`;
 }
